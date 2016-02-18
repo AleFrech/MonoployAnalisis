dcf0b0e60164c1ad73a1b71d9559c7a099a0f29e
edwherrera <edwherrera.175@gmail.com>
2016-02-18 15:36:01 -0600

    PlayerPiece enum added

--- MonoployAnalisis/Player.cs
+++ MonoployAnalisis/Player.cs
@@ -8,5 +8,24 @@ namespace MonoployAnalisis
 {
     class Player
     {
+        public enum PlayerPiece
+        {
+            CAR,
+            DOG,
+            MONEY_BAG,
+            WHEEL_CART
+        };
+
+        private PlayerPiece _piece;
+        public PlayerPiece Piece
+        {
+            get { return this._piece; }
+        }
+
+        public Player(PlayerPiece piece)
+        {
+            this._piece = piece;
+        }
+
     }
 }
