e12a4e6188cc3febaaa77faa0e0576cd8452e634
Mario V <mvilla220@gmail.com>
2016-02-19 15:26:21 -0600

    Dice, layout finished

--- MonoployAnalisis/Form1.Designer.cs
+++ MonoployAnalisis/Form1.Designer.cs
@@ -1,4 +1,9 @@
-﻿namespace MonoployAnalisis
+﻿using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+
+namespace MonoployAnalisis
 { 
     partial class Form1
     {
@@ -28,196 +33,215 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.comboBox2 = new System.Windows.Forms.ComboBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
-            this.label4 = new System.Windows.Forms.Label();
-            this.pictureBox4 = new System.Windows.Forms.PictureBox();
-            this.pictureBox5 = new System.Windows.Forms.PictureBox();
-            this.button1 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            this.panel1.SuspendLayout();
-            this.panel2.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.board = new System.Windows.Forms.PictureBox();
+            this.nameP1 = new System.Windows.Forms.Label();
+            this.propertiesP1 = new System.Windows.Forms.ComboBox();
+            this.pieceP1 = new System.Windows.Forms.PictureBox();
+            this.fundsP1 = new System.Windows.Forms.Label();
+            this.panelP1 = new System.Windows.Forms.Panel();
+            this.dice1 = new System.Windows.Forms.PictureBox();
+            this.dice2 = new System.Windows.Forms.PictureBox();
+            this.rollDice = new System.Windows.Forms.Button();
+            this.panelP2 = new System.Windows.Forms.Panel();
+            this.propertiesP2 = new System.Windows.Forms.ComboBox();
+            this.pieceP2 = new System.Windows.Forms.PictureBox();
+            this.fundsP2 = new System.Windows.Forms.Label();
+            this.nameP2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.board)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pieceP1)).BeginInit();
+            this.panelP1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dice1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dice2)).BeginInit();
+            this.panelP2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pieceP2)).BeginInit();
             this.SuspendLayout();
             // 
-            // pictureBox1
+            // board
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(883, 706);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
+            this.board.Image = global::MonoployAnalisis.Properties.Resources.board;
+            this.board.Location = new System.Drawing.Point(12, 12);
+            this.board.Name = "board";
+            this.board.Size = new System.Drawing.Size(680, 680);
+            this.board.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.board.TabIndex = 0;
+            this.board.TabStop = false;
+            this.board.Click += new System.EventHandler(this.board_Click);
             // 
-            // label1
+            // nameP1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(14, 21);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
+            this.nameP1.AutoSize = true;
+            this.nameP1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nameP1.Location = new System.Drawing.Point(15, 9);
+            this.nameP1.Name = "nameP1";
+            this.nameP1.Size = new System.Drawing.Size(78, 25);
+            this.nameP1.TabIndex = 1;
+            this.nameP1.Text = "Player1";
             // 
-            // comboBox1
+            // propertiesP1
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(17, 37);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 21);
-            this.comboBox1.TabIndex = 2;
+            this.propertiesP1.FormattingEnabled = true;
+            this.propertiesP1.Location = new System.Drawing.Point(17, 37);
+            this.propertiesP1.Name = "propertiesP1";
+            this.propertiesP1.Size = new System.Drawing.Size(121, 21);
+            this.propertiesP1.TabIndex = 2;
             // 
-            // pictureBox2
+            // pieceP1
             // 
-            this.pictureBox2.Location = new System.Drawing.Point(17, 64);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(76, 69);
-            this.pictureBox2.TabIndex = 3;
-            this.pictureBox2.TabStop = false;
+            this.pieceP1.Location = new System.Drawing.Point(17, 64);
+            this.pieceP1.Name = "pieceP1";
+            this.pieceP1.Size = new System.Drawing.Size(76, 69);
+            this.pieceP1.TabIndex = 3;
+            this.pieceP1.TabStop = false;
             // 
-            // label2
+            // fundsP1
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(99, 93);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
-            this.label2.TabIndex = 4;
-            this.label2.Text = "label2";
+            this.fundsP1.AutoSize = true;
+            this.fundsP1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fundsP1.ForeColor = System.Drawing.Color.Lime;
+            this.fundsP1.Location = new System.Drawing.Point(99, 93);
+            this.fundsP1.Name = "fundsP1";
+            this.fundsP1.Size = new System.Drawing.Size(102, 31);
+            this.fundsP1.TabIndex = 4;
+            this.fundsP1.Text = "$ 1000";
+            this.fundsP1.Click += new System.EventHandler(this.fundsP1_Click);
             // 
-            // panel1
+            // panelP1
             // 
-            this.panel1.Controls.Add(this.comboBox1);
-            this.panel1.Controls.Add(this.label1);
-            this.panel1.Controls.Add(this.pictureBox2);
-            this.panel1.Controls.Add(this.label2);
-            this.panel1.Location = new System.Drawing.Point(914, 572);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(200, 146);
-            this.panel1.TabIndex = 9;
-            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panelP1.Controls.Add(this.propertiesP1);
+            this.panelP1.Controls.Add(this.nameP1);
+            this.panelP1.Controls.Add(this.pieceP1);
+            this.panelP1.Controls.Add(this.fundsP1);
+            this.panelP1.Location = new System.Drawing.Point(783, 546);
+            this.panelP1.Name = "panelP1";
+            this.panelP1.Size = new System.Drawing.Size(223, 146);
+            this.panelP1.TabIndex = 9;
+            this.panelP1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panelP1.BorderStyle = BorderStyle.FixedSingle;
             // 
-            // panel2
+            // dice1
             // 
-            this.panel2.Controls.Add(this.comboBox2);
-            this.panel2.Controls.Add(this.label3);
-            this.panel2.Controls.Add(this.pictureBox3);
-            this.panel2.Controls.Add(this.label4);
-            this.panel2.Location = new System.Drawing.Point(1138, 572);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(200, 146);
-            this.panel2.TabIndex = 10;
+            this.dice1.Image = ((System.Drawing.Image)(resources.GetObject("dice1.Image")));
+            this.dice1.Location = new System.Drawing.Point(920, 458);
+            this.dice1.Name = "dice1";
+            this.dice1.Size = new System.Drawing.Size(86, 82);
+            this.dice1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.dice1.TabIndex = 11;
+            this.dice1.TabStop = false;
             // 
-            // comboBox2
+            // dice2
             // 
-            this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(17, 37);
-            this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(121, 21);
-            this.comboBox2.TabIndex = 2;
+            this.dice2.Image = global::MonoployAnalisis.Properties.Resources.r1;
+            this.dice2.Location = new System.Drawing.Point(1058, 458);
+            this.dice2.Name = "dice2";
+            this.dice2.Size = new System.Drawing.Size(86, 82);
+            this.dice2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.dice2.TabIndex = 12;
+            this.dice2.TabStop = false;
             // 
-            // label3
+            // rollDice
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(14, 21);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(35, 13);
-            this.label3.TabIndex = 1;
-            this.label3.Text = "label3";
+            this.rollDice.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rollDice.Location = new System.Drawing.Point(969, 399);
+            this.rollDice.Name = "rollDice";
+            this.rollDice.Size = new System.Drawing.Size(122, 53);
+            this.rollDice.TabIndex = 13;
+            this.rollDice.Text = "Roll Dice!";
+            this.rollDice.UseVisualStyleBackColor = true;
+            this.rollDice.Click += new System.EventHandler(this.rollDice_Click);
             // 
-            // pictureBox3
+            // panelP2
             // 
-            this.pictureBox3.Location = new System.Drawing.Point(17, 64);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(76, 69);
-            this.pictureBox3.TabIndex = 3;
-            this.pictureBox3.TabStop = false;
+            this.panelP2.Controls.Add(this.nameP2);
+            this.panelP2.Controls.Add(this.propertiesP2);
+            this.panelP2.Controls.Add(this.pieceP2);
+            this.panelP2.Controls.Add(this.fundsP2);
+            this.panelP2.Location = new System.Drawing.Point(1058, 546);
+            this.panelP2.Name = "panelP2";
+            this.panelP2.Size = new System.Drawing.Size(223, 146);
+            this.panelP2.TabIndex = 10;
+            this.panelP2.BorderStyle = BorderStyle.FixedSingle;
             // 
-            // label4
+            // propertiesP2
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(99, 93);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(35, 13);
-            this.label4.TabIndex = 4;
-            this.label4.Text = "label4";
+            this.propertiesP2.FormattingEnabled = true;
+            this.propertiesP2.Location = new System.Drawing.Point(17, 37);
+            this.propertiesP2.Name = "propertiesP2";
+            this.propertiesP2.Size = new System.Drawing.Size(121, 21);
+            this.propertiesP2.TabIndex = 2;
             // 
-            // pictureBox4
+            // pieceP2
             // 
-            this.pictureBox4.Location = new System.Drawing.Point(1028, 474);
-            this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(86, 82);
-            this.pictureBox4.TabIndex = 11;
-            this.pictureBox4.TabStop = false;
+            this.pieceP2.Location = new System.Drawing.Point(17, 64);
+            this.pieceP2.Name = "pieceP2";
+            this.pieceP2.Size = new System.Drawing.Size(76, 69);
+            this.pieceP2.TabIndex = 3;
+            this.pieceP2.TabStop = false;
             // 
-            // pictureBox5
+            // fundsP2
             // 
-            this.pictureBox5.Location = new System.Drawing.Point(1138, 474);
-            this.pictureBox5.Name = "pictureBox5";
-            this.pictureBox5.Size = new System.Drawing.Size(86, 82);
-            this.pictureBox5.TabIndex = 12;
-            this.pictureBox5.TabStop = false;
+            this.fundsP2.AutoSize = true;
+            this.fundsP2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fundsP2.ForeColor = System.Drawing.Color.Lime;
+            this.fundsP2.Location = new System.Drawing.Point(99, 93);
+            this.fundsP2.Name = "fundsP2";
+            this.fundsP2.Size = new System.Drawing.Size(102, 31);
+            this.fundsP2.TabIndex = 4;
+            this.fundsP2.Text = "$ 1000";
             // 
-            // button1
+            // nameP2
             // 
-            this.button1.Location = new System.Drawing.Point(1086, 445);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 13;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.nameP2.AutoSize = true;
+            this.nameP2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nameP2.Location = new System.Drawing.Point(15, 9);
+            this.nameP2.Name = "nameP2";
+            this.nameP2.Size = new System.Drawing.Size(78, 25);
+            this.nameP2.TabIndex = 5;
+            this.nameP2.Text = "Player2";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1350, 690);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.pictureBox5);
-            this.Controls.Add(this.pictureBox4);
-            this.Controls.Add(this.panel2);
-            this.Controls.Add(this.panel1);
-            this.Controls.Add(this.pictureBox1);
+            this.ClientSize = new System.Drawing.Size(1350, 750);
+            this.Controls.Add(this.panelP2);
+            this.Controls.Add(this.rollDice);
+            this.Controls.Add(this.dice2);
+            this.Controls.Add(this.dice1);
+            this.Controls.Add(this.panelP1);
+            this.Controls.Add(this.board);
             this.Name = "Form1";
             this.Text = "Monopoly";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
-            this.panel2.ResumeLayout(false);
-            this.panel2.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.board)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pieceP1)).EndInit();
+            this.panelP1.ResumeLayout(false);
+            this.panelP1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dice1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dice2)).EndInit();
+            this.panelP2.ResumeLayout(false);
+            this.panelP2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pieceP2)).EndInit();
             this.ResumeLayout(false);
 
         }
         #endregion
 
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.ComboBox comboBox2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.PictureBox pictureBox3;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.PictureBox pictureBox4;
-        private System.Windows.Forms.PictureBox pictureBox5;
-        private System.Windows.Forms.Button button1;
+        private PictureBox board;
+        private Label fundsP1;
+        private Panel panelP1;
+        private PictureBox pieceP1;
+        private ComboBox propertiesP1;
+        private Label nameP1;
+        private PictureBox dice1;
+        private PictureBox dice2;
+        private Button rollDice;
+        private Panel panelP2;
+        private Label nameP2;
+        private ComboBox propertiesP2;
+        private PictureBox pieceP2;
+        private Label fundsP2;
     }
 }
 
--- MonoployAnalisis/Form1.cs
+++ MonoployAnalisis/Form1.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows.Forms;
+using MonoployAnalisis.Properties;
+
 
 namespace MonoployAnalisis
 {
@@ -23,6 +19,86 @@ namespace MonoployAnalisis
         }
 
         private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void board_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public Tuple<int, int> RollDiceValueTuple()
+        {
+            Random value = new Random();
+            int diceValue1 = value.Next(1, 6);
+            int diceValue2 = value.Next(1, 6);
+            return Tuple.Create(diceValue1, diceValue2);
+        }
+
+        private void rollDice_Click(object sender, EventArgs e)
+        {
+            int diceImage1 = RollDiceValueTuple().Item1;
+            int diceImage2 = RollDiceValueTuple().Item2;
+
+            System.Diagnostics.Debug.WriteLine("value 1:" + diceImage1);
+            System.Diagnostics.Debug.WriteLine("value 2:" + diceImage2);
+
+            switch (diceImage1)
+            {
+                case 1:
+                    dice1.Image = global::MonoployAnalisis.Properties.Resources.p1;
+                    break;
+                case 2:
+                    dice1.Image = global::MonoployAnalisis.Properties.Resources.p2;
+                    break;
+                case 3:
+                    dice1.Image = global::MonoployAnalisis.Properties.Resources.p3;
+                    break;
+                case 4:
+                    dice1.Image = global::MonoployAnalisis.Properties.Resources.p4;
+                    break;
+                case 5:
+                    dice1.Image = global::MonoployAnalisis.Properties.Resources.p5;
+                    break;
+                case 6:
+                    dice1.Image = global::MonoployAnalisis.Properties.Resources.p6;
+                    break;
+                default:
+                    Console.WriteLine("No pos guau...");
+                    break;
+
+            }
+
+
+            switch (diceImage2)
+            {
+                case 1:
+                    dice2.Image = global::MonoployAnalisis.Properties.Resources.r1;
+                    break;
+                case 2:
+                    dice2.Image = global::MonoployAnalisis.Properties.Resources.r2;
+                    break;
+                case 3:
+                    dice2.Image = global::MonoployAnalisis.Properties.Resources.r3;
+                    break;
+                case 4:
+                    dice2.Image = global::MonoployAnalisis.Properties.Resources.r4;
+                    break;
+                case 5:
+                    dice2.Image = global::MonoployAnalisis.Properties.Resources.r5;
+                    break;
+                case 6:
+                    dice2.Image = global::MonoployAnalisis.Properties.Resources.r6;
+                    break;
+                default:
+                    Console.WriteLine("No pos guau...");
+                    break;
+
+            }
+        }
+
+        private void fundsP1_Click(object sender, EventArgs e)
         {
 
         }
