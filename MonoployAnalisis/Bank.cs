5219ebbf701a899e2d169808bc8bdf996b950c88
edwherrera <edwherrera.175@gmail.com>
2016-02-18 18:20:01 -0600

    TransferFunds method added

--- MonoployAnalisis/Bank.cs
+++ MonoployAnalisis/Bank.cs
@@ -8,5 +8,21 @@ namespace MonoployAnalisis
 {
     class Bank
     {
+        private static int GO = 200;
+
+        public static bool TransferFunds(Player fromPlayer, Player toPlayer, int amount, bool reThrows) 
+        {
+            try
+            {
+                fromPlayer.TransferFunds(toPlayer, amount);
+            }
+            catch (InsufficientFundsException ex)
+            {
+                if (reThrows) { throw ex; }
+                return false;
+            }
+
+            return true;
+        }
     }
 }
