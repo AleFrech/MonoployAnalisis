eb3dc51590c3da3bbd5380fd98eca084f0f85ac4
Alejandro-Frech <alefrech@gmail.com>
2016-02-19 14:11:51 -0600

    Implement GO

---
+++ MonoployAnalisis/Go.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonoployAnalisis
+{
+    public class Go:BoardObject
+    {
+        public Go() : base("Go")
+        {
+            
+        }
+
+
+        public void AddGo(Player player)
+        {
+            Bank.AddGo(player);
+        }
+    }
+}
